0ab3fa8d5ee2c4491a4d832f186bc8bc2bb88b31
Mohamed Farouk <mohamedfaroukelmekawy@gmail.com>
2026-02-04 15:05:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,7 +7,15 @@ app = FastAPI()
 def home():
     return {"message": "Welcome to my FastAPI project"}
 
-
 @app.get("/hello/{name}")
 def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+
+class Message(BaseModel):
+    text: str
+
+@app.post("/echo")
+def echo_message(message: Message):
+    return {"response": message.text}
